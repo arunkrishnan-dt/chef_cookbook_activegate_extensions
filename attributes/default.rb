3319dc78358218ae03f9670adaf78945ec04ec2a
Arun Krishnan <arun.krishnan>
2022-02-03 15:41:34 +1300

    extension exist logic moved to helper lib

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,18 +1,13 @@
-default['download_repo'] = 'https://dt-cdn.net/hub/extensions'
+default['extension_repo'] = "https://dt-cdn.net/hub/extensions"
 default['activegate']['extension_dir'] = '/opt/dynatrace/remotepluginmodule/plugin_deployment'
 default['activegate']['tmp_dir'] = '/var/tmp'
 
 default['dynatrace']['tenancy_url'] = "https://xzv52984.live.dynatrace.com"
 default['tenancy']['uid'] = 'xzv52984'
 
-# default values
-default['extension']['name'] = 'ibmmq_j'
-default['extension']['version'] = '2.0.0'
-default['extension']['os'] = 'linux'
-
 default['extensions'] = {
     'ibmmq_java' => { 
-        'version' => '2.021.1',
+        'version' => '2.020.10',
         'os' => ''
     },
     'mssql' => {
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,70 +4,38 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
-# Check Version if file on host
+# Do below for all extensions
 node['extensions'].each do |name, properties|    
     
     log 'attribute_file_version' do
-        message "Attribute file version: #{name} - #{properties['version']}"
-        level :info
+        message "Attribute file version: #{name} - #{properties['version']} - #{node.default['extension_repo']} "
+        level :info        
     end    
 
-    if File.exist?("#{node['activegate']['extension_dir']}/custom.remote.python.#{name}/plugin.json")
-        File.open("#{node['activegate']['extension_dir']}/custom.remote.python.#{name}/plugin.json") do |f|                    
-            data_hash = JSON.parse(f.read)                    
+    # Install Extension on ActiveGate
+    activegate_extensions_install_extension "#{name}" do
+        extension_repo "#{node.default['extension_repo']}"
+        version "#{properties['version']}"                       
+        os "#{properties['os']}"        
+        extension_dir "#{node.default['activegate']['extension_dir']}"
+        tmp_dir "#{node.default['activegate']['tmp_dir']}"        
+        action :install
+        extend ActivegateExtensions::Exists
+        only_if {install_ext?("#{node['activegate']['extension_dir']}/custom.remote.python.#{name}/plugin.json", "#{properties['version']}")}        
+    end    
 
-            log 'server_version' do
-                message "Existing server version: #{name} - #{data_hash['version']}"
-                level :info
-            end 
+    # Upload Extension to tenancy
+    activegate_extensions_upload_extension 'upload-ext' do        
+        name "#{name}"
+        version "#{properties['version']}"                
+        tmp_dir "#{node.default['activegate']['tmp_dir']}"        
+        tenancy_url "#{node.default['dynatrace']['tenancy_url']}"
+        api_token "#{node.default['dynatrace']['api_token']}" # Token attribute file is local and not in repository
+        action :upload
+    end
 
-            # Update extension - if exists & not on required version
-            if data_hash['version'] != "#{properties['version']}"
-                activegate_extensions_install_extension "#{name}" do
-                    version "#{properties['version']}"                       
-                    os "#{properties['os']}"
-                    download_repo "#{node.default['download_repo']}"
-                    extension_dir "#{node.default['activegate']['extension_dir']}"
-                    tmp_dir "#{node.default['activegate']['tmp_dir']}"        
-                    action :install
-                end    
-
-                activegate_extensions_upload_extension "#{name}" do
-                    version "#{properties['version']}"                
-                    tmp_dir "#{node.default['activegate']['tmp_dir']}"        
-                    tenancy_url "#{node.default['dynatrace']['tenancy_url']}"
-                    api_token "#{node.default['dynatrace']['api_token']}" # Token attribute file is local and not in repository
-                    action :upload
-                end
-
-                log 'updated_version' do
-                    message "Extension updated to  #{name} - #{properties['version']}"
-                    level :info
-                end 
-            end
-        end
-    else
-        # Add new extension
-        activegate_extensions_install_extension "#{name}" do
-            version "#{properties['version']}"                       
-            os "#{properties['os']}"
-            download_repo "#{node.default['download_repo']}"
-            extension_dir "#{node.default['activegate']['extension_dir']}"
-            tmp_dir "#{node.default['activegate']['tmp_dir']}"        
-            action :install
-        end    
-
-        activegate_extensions_upload_extension "#{name}" do
-            version "#{properties['version']}"                
-            tmp_dir "#{node.default['activegate']['tmp_dir']}"        
-            tenancy_url "#{node.default['dynatrace']['tenancy_url']}"
-            api_token "#{node.default['dynatrace']['api_token']}" # Token attribute file is local and not in repository
-            action :upload
-        end
-
-        log 'installed_version' do
-            message "Installed extension #{name} - #{properties['version']}"
-            level :info
-        end
-    end      
+    log 'updated_version' do
+        message "Extension updated to  #{name} - #{properties['version']}"
+        level :info
+    end    
 end
